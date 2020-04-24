79399a921d1383bb2c8420191d9ba2a4d516f673
zac-freeman <zpfreeman14@gmail.com>
2020-04-23 21:36:23 -0400

    moved unused code to graveyard, updated README

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # html-make.sh
- A script intended to enable templating of HTML without the use of javascript. Makes use of common GNU tools.
+A Perl (five) script designed to enable templating of HTML without the use of javascript. Truthfully, it could be used to enable templating for any kind of text file, but HTML is one of few languages that could make use of templating and has no native support for such a thing (Rest in Peace HTML Imports, 2011-2016ish)
 
- ## this is a work in progress
- Currently, this script only supports one component, one template, and one final file. Soon, support for multiple of each will be added. Further down the road is nested components and component parameters.
+## this is a work in progress
+Currently, this project is in an experimental phase. As of the last update to this README, the html-make.pl script supports populating a hash of templates with their declared dependencies. The initial goal is to read files at a given location, populate them, name them, and write them to another given location.
 
- Documentation and samples are in the works, as well.
+Documentation and samples are in the works, as well.
+
+## the graveyard
+The graveyard is where I put things that should be deleted. In a sense, this codebase is haunted by dead code.
--- html-make.pl
+++ html-make.pl
@@ -1,84 +1,3 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-#for a file at a given path, returns its content
-sub getFileContent {
-	my $path = $_[0];
-
-	open(my $handle, $path);
-	my $content = "";
-	while (my $line = <$handle>) {
-		$content = "$content$line";
-	}
-
-	chomp($content);
-	close($handle);
-	return $content
-}
-
-#for a folder at a given path, returns a map of file names to file contents
-sub getFolderContent {
-	my $path = $_[0];
-
-	my $dir;
-	opendir($dir, $path);
-
-	my %files;
-	while (my $file = readdir($dir)) {
-		my $filePath = "$path/$file";
-		
-		#if it is a file, check if the file name is unique,
-		#if it is unique, grab its content and store it in the map
-		#if it isn't unique, throw an error about a file name collision
-		if (-f $filePath) {
-			if (exists($files{$file})) {
-				die("ERROR: Found multiple files with name \"$file\"\n");
-			}
-			my $content = getContent($filePath);
-			$files{$file} = $content;
-		}
-		#if it is a directory other than the relative directories (., ..),
-		#get the files from it and store them in the map
-		elsif (-d $filePath and $file ne "." and $file ne ".." ) {
-			my %grandfiles = getFiles($filePath);
-			%files = (%files, %grandfiles);
-		}
-	}
-
-	closedir($dir);
-	return %files;
-}
-
-#for a given file content and a given map reference, finds instances of [[*]],
-#replaces it with the corresponding file content, and returns the updated file content
-sub findAndReplace {
-	my $content = $_[0];
-	my $files = $_[1];
-
-	while ($content =~ /\[\[[0-9a-zA-Z._\-]+\]\]/) {
-		my $start = $-[0];
-		my $end = $+[0];
-		my $file = substr($content, $start + 2, $end - $start - 4);
-		if (!exists($files->{$file})) {
-			die("ERROR: No file found with name \"$file\"\n");
-		}
-		$files->{$file} = findAndReplace($files->{$file}, $files);
-		$content = substr($content, 0, $start) . $files->{$file} . substr($content, $end);
-	}
-	return $content;
-}
-
-
-my $path = $ARGV[0];
-my %files = getFiles($path);
-
-findAndReplace("[[template.html]]", \%files);
-
-while((my $file, my $content) = each(%files)) {
-	print("\n--------\n$file:\n$content\n");
-}
-
 # new function organization:
 #
 #	Keep IO and non-IO functions seperate (haskell, eat your heart out) for testing purposes. I think
@@ -167,7 +86,7 @@ sub populateTemplate {
 
 		# if the parents array is empty, add dependencyName to the end of it
 		if (@parents != 0) {
-			push (@parents, $dependencyName);
+			push(@parents, $dependencyName);
 		}
 
 		# get a reference to the contents corresponding to the dependencyName,
